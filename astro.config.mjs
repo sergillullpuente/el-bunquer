9e4614d35872cfbb7ca0a9840475c34387032c38
Sergi Llull Puente <sergillullpuente@gmail.com>
2024-12-03 15:25:40 +0100

    refactor: use catalan as lang in html, remove unused netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
-import netlify from '@astrojs/netlify';
-
 export default defineConfig({
-  output: 'hybrid',
-
-  integrations: [    tailwind({
-      applyBaseStyles: false,
-      nesting: true
-  }), preact()],
-
-  adapter: netlify(),
+  integrations: [
+      tailwind({
+          applyBaseStyles: false,
+          nesting: true
+      }),
+      preact()
+  ],
 });
